bc57d0dd65ba06de67dfdc5acb434681152959dd
W3n2il3 <pearlbathobile.com>
2024-05-27 12:36:42 -0700

    prop

---
+++ 0x02-react_props/task_0/dashboard/src/Footer/Footer.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import { getFooterCopy, getFullYear } from '../utils';
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='App-footer'>
+        <p><em>Copyright { getFullYear() } - { getFooterCopy(true) } </em></p>
+    </div>
+  )
+}
+
+export default Footer
+
